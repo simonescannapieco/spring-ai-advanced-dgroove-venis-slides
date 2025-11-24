e769d8d5c4598c940a07177b7e281708c557a4a4
Simone Scannapieco <simone.scannapieco@gmail.com>
2025-11-24 09:39:39 +0100

    Stub for text to speech

---
+++ practice/38_spring_ai_gemini_ollama_multimodality_text_to_speech/code/TextToSpeechModel.java
@@ -0,0 +1,25 @@
+/**
+ * Interface for the text to speech model.
+ *
+ * @author Alexandros Pappas
+ */
+public interface TextToSpeechModel extends Model<TextToSpeechPrompt, TextToSpeechResponse>, StreamingTextToSpeechModel {
+
+	default byte[] call(String text) {
+		TextToSpeechPrompt prompt = new TextToSpeechPrompt(text);
+		ModelResult<byte[]> result = call(prompt).getResult();
+		if (result == null) {
+			return new byte[0];
+		}
+		byte[] output = result.getOutput();
+		return (output != null) ? output : new byte[0];
+	}
+
+	@Override
+	TextToSpeechResponse call(TextToSpeechPrompt prompt);
+
+	default TextToSpeechOptions getDefaultOptions() {
+		return TextToSpeechOptions.builder().build();
+	}
+
+}
